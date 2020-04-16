de036c60d397674dd9057311b7274b8c101c3b82
jlazovskis <jlazovskis@gmail.com>
2020-04-16 10:39:19 +0100

    argparse introduced for options

--- nest_mc2.py
+++ nest_mc2.py
@@ -1,17 +1,29 @@
 # Biological neural network model in NEST simulator. Based on BlueBrain neocortical microcircuit and experiments from Frontiers paper.
+# Neuro-Topology Group, Institute of Mathematics, University of Aberdeen
 # Authors: Jānis Lazovskis, Jason Smith
 # Date: March 2020
 
 # Import packages
 import pylab
 import itertools
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import numpy as np
+import matplotlib as mpl                                                    # For plotting
+import matplotlib.pyplot as plt                                             # For plotting
+from mpl_toolkits.axes_grid1 import make_axes_locatable                     # For plotting, to add colorbar nicely
+import numpy as np                                                          # For reading of files
 import sys
-import nest
+import nest                                                                 # For main simulation
+import argparse                                                             # For options
 
+# Read arguments
+parser = argparse.ArgumentParser(
+    description='Simplified Blue Brain Project reconstructions and validations',
+    usage='python nest_mc2.py [--fibres=fibres.npy] [--stimulus=stim.npy] [--title="Plot title"]')
+parser.add_argument('--fibres', type=str, default='fibres.npy', help='Filename of fibres in folder "stimuli". List of lists of GIDs. Length is number of thalamic fibers.')
+parser.add_argument('--stimulus', type=str, default='constant_firing.npy', help='Filename of firing pattern of stimulus in folder "stimuli". List of tuples (index,start,stop,rate). Index has to be within range of length of fibres option.')
+parser.add_argument('--title', type=str, default='Spikemeter and voltmeter reports', help='Title for plot produced at the end.')
+args = parser.parse_args()
+
+# Set up
 nest.ResetKernel()                                                          # Reset nest
 root = sys.argv[0][:-11]                                                    # Current working directory
                                         
@@ -22,9 +34,10 @@ mc2_edges = list(zip(adj['row'], adj['col']))                               # Ge
 nnum = 31346                                                                # Number of neurons in circuit
 
 # Load stimulus info
-fibres_address = root+'stimuli/fibres.npy'                                  # Address of thalamic nerve fibres data
+fibres_address = root+'stimuli/'+args.fibres                                # Address of thalamic nerve fibres data
 fibres = np.load(fibres_address,allow_pickle=True)                          # Get which neurons the fibres connect to
-firing_pattern = [(i,i,i+1.1,2) for i in range(len(fibres))]                # When the nerve fibres fire, each element of the form (fibre_id,start_time,end_time,firing_rate)
+stimulus_address = root+'stimuli/'+args.stimulus
+firing_pattern = np.load(stimulus_address,allow_pickle=True)                # When the nerve fibres fire, each element of the form (fibre_id,start_time,end_time,firing_rate)
 stim_strength = 1000000
 
 # Declare parameters
@@ -81,7 +94,7 @@ stims = nest.GetStatus(spikedetector)[0]['events']
 volts = nest.GetStatus(voltmeter)[0]['events']['V_m']
 
 fig = plt.figure(figsize=(15,5)) # default is (8,6)
-fig.suptitle('Spikemeter and voltmeter reports',fontsize=18)
+fig.suptitle(args.title,fontsize=18)
 
 ax_stim = fig.add_subplot(2,1,1)
 ax_stim.scatter(stims['times'], stims['senders'], s=1, marker="+")
