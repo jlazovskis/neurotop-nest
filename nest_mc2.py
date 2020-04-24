36cc29418ab69a5fe96bf37e36f3ed8734015cae
jlazovskis <jlazovskis@gmail.com>
2020-04-24 21:31:54 +0100

    flagser extension, adjusted plots

--- nest_mc2.py
+++ nest_mc2.py
@@ -14,10 +14,12 @@ import argparse                                                             # Fo
 # Packages: for outputs
 from datetime import datetime                                               # For giving messages
 import h5py                                                                 # For exporting h5 files
+from functools import reduce                                                # For combining simplices
 import matplotlib as mpl                                                    # For plotting
 import matplotlib.pyplot as plt                                             # For plotting
 from mpl_toolkits.axes_grid1 import make_axes_locatable                     # For plotting, to add colorbar nicely
 import scipy.sparse                                                         # For exporting transimssion response matrices
+import subprocess                                                           # For counting simplices and running flagser
 
 # Auxiliary: Formatted printer for messages
 def ntnstatus(message):
@@ -45,11 +47,13 @@ parser.add_argument('--time', type=int, default=100, help='Length, in millisecon
 
 # Arguments: outputs
 parser.add_argument('--make_spikes', action='store_true', help='If included, outputs a file "spikes.h5" containing two lists, one of the GIDs of spiking neurons, the other of times at which they spiked. This is the dictionary NEST produces with getstatus for the events of the spikedetector.')
-parser.add_argument('--make_tr', action='store_true', help='If included, outputs a file "tr.npz" containing transmission reponse matrices.')
+parser.add_argument('--make_tr', action='store_true', help='If included, outputs a file "tr.npz" containing transmission response matrices.')
+parser.add_argument('--count_simplices', action='store_true', help='If included, outputs a file "simplexcount.npy" containing simplex counts for each transmission response step.')
+parser.add_argument('--flagser', type=str, default='../flagser/flagser', help='Location of flagser executable.')
 parser.add_argument('--t1', type=float, default=5.0, help='t1 for transmission reponse matrices')
 parser.add_argument('--t2', type=float, default=10.0, help='t2 for transmission reponse matrices')
 parser.add_argument('--no_plot', action='store_true', help='If not included, outputs visual plot of spikes and voltages of experiment.')
-parser.add_argument('--outplottitle', type=str, default='Spikemeter and voltmeter reports', help='Title for plot produced at the end. ')
+parser.add_argument('--outplottitle', type=str, default='Visual reports', help='Title for plot produced at the end. ')
 args = parser.parse_args()
 
 # Set up
@@ -145,8 +149,8 @@ if args.make_spikes:
 		f.create_dataset(k, data=spikes[k])
 	f.close()
 
-# Report: transmission response matrices
-if args.make_tr:
+# Report: transmission response matrices and simplex count
+if args.make_tr or args.count_simplices:
 	t1 = args.t1
 	t2 = args.t2
 	ntnstatus("Creating transmission response matrices with t1="+str(t1)+" and t2="+str(t2))
@@ -155,12 +159,13 @@ if args.make_tr:
 	# Get ordered times and indices of spikes
 	spikes = nest.GetStatus(spikedetector)[0]['events']
 	tr_times = sorted(spikes['times'])
-	tr_neurons = [x for _,x in sorted(zip(spikes['times'], spikes['senders']))]
+	tr_neurons = [x-1 for _,x in sorted(zip(spikes['times'], spikes['senders']))]
 	# Get adjacencies as dense matrix
 	adjmat = scipy.sparse.coo_matrix((adj['data'], (adj['row'],adj['col'])), shape=(nnum,nnum)).toarray()
 	matrices = []
+	simplices = []
 	for i in range(len(times)-1):
-		print('    Step '+str(i)+' as arr_'+str(i)+': ['+str(times[i])+','+str(times[i+1])+'] in ['+str(times[i])+','+str(min(times[i]+t2,exp_length))+']',flush=True)
+		print('    Step '+str(i)+': ['+str(times[i])+','+str(times[i+1])+'] in ['+str(times[i])+','+str(min(times[i]+t2,exp_length))+']',flush=True)
 		M = np.zeros((nnum,nnum),dtype='int8')
 		t1_start = np.searchsorted(tr_times, times[i])
 		t1_end = np.searchsorted(tr_times, times[i+1])
@@ -170,36 +175,74 @@ if args.make_tr:
 		targets = np.unique(tr_neurons[t1_start:t2_end])
 		target_vector = scipy.sparse.coo_matrix((np.ones(len(targets),dtype='int8'), (np.zeros(len(targets),dtype='int8'), targets)), shape=(1,nnum+1)).toarray()[0][1:]
 		for source in sources:
-			M[source-1] = np.logical_and(adjmat[source-1],target_vector)
+			M[source] = np.logical_and(adjmat[source],target_vector)
 		# Source vertex spiked in [t1,t2], sink in [t1,t2]
 		sources = np.unique(tr_neurons[t1_end:t2_end])
 		target_vector = scipy.sparse.coo_matrix((np.ones(len(sources),dtype='int8'), (np.zeros(len(sources),dtype='int8'), sources)), shape=(1,nnum+1)).toarray()[0][1:]
 		for source in sources:
-			M[source-1] = np.logical_and(adjmat[source-1],target_vector)
+			M[source] = np.logical_and(adjmat[source],target_vector)
 		matrices.append(M)
-	print('    Compressing '+str(len(matrices))+' dense matrices into npz file',flush=True)
-	np.savez_compressed(root+'transmissionresponse_'+timestamp, *matrices)
+		if args.count_simplices:
+			f = open(root+'step'+str(i),'w')
+			f.write('dim 0\n')
+			for j in range(nnum):
+				f.write('0 ')
+			f.write('\ndim 1\n')
+			for j in range(nnum):
+				for k in np.nonzero(matrices[i][j])[0]:
+					f.write(str(j)+' '+str(k)+'\n')
+			f.close()
+			cmd = subprocess.Popen(['./'+str(args.flagser), '--out', root+'step'+str(i)+'.flag',  root+'step'+str(i)], stdout=subprocess.DEVNULL)
+			cmd.wait()
+			g = open(root+'step'+str(i)+'.flag','r')
+			L = g.readlines()
+			g.close()
+			simplices.append(np.array(list(map(lambda x: int(x), L[1][:-1].split(' ')[1:]))))
+			print('    Simplex counts > 1: '+reduce((lambda x,y: str(x)+' '+str(y)), simplices[-1]), flush=True)
+			subprocess.Popen(['rm', root+'step'+str(i)], stdout=subprocess.DEVNULL)
+			subprocess.Popen(['rm', root+'step'+str(i)+'.flag'], stdout=subprocess.DEVNULL)
+	if args.make_tr:
+		ntnstatus('Compressing '+str(len(matrices))+' dense matrices into npz file')
+		np.savez_compressed(root+'transmissionresponse_'+timestamp, *matrices)
+	if args.count_simplices:
+		maxlen = max([len(s) for s in simplices])
+		for s in range(len(simplices)):
+			while len(simplices[s]) < maxlen:
+				simplices[s] = np.concatenate((simplices[s],np.zeros(1,dtype='int64')), axis=None)
+		np.save(root+'simplices_'+timestamp, np.array(simplices))
 
 # Report: spike and voltage plots
 if not args.no_plot:
 	ntnstatus("Creating spike and volt plots")
 	spikes = nest.GetStatus(spikedetector)[0]['events']
 	volts = nest.GetStatus(voltmeter)[0]['events']['V_m']
-	fig = plt.figure(figsize=(15,5)) # default is (8,6)
+	fig = plt.figure(figsize=(15,6)) # default is (8,6)
 	fig.suptitle(args.outplottitle,fontsize=18)
 	
-	ax_spikes = fig.add_subplot(2,1,1)
+	if args.count_simplices:
+		ax_spikes = fig.add_subplot(3,1,1)
+		ax_volts = fig.add_subplot(3,1,2)
+		ax_simp = fig.add_subplot(3,1,3); plt.yscale("symlog")
+		for dim in range(len(simplices[0])-1):
+			ax_simp.plot(range(len(simplices)), [simplices[step][dim] for step in range(len(simplices))], label='dim'+str(dim+1))
+		ax_simp.legend(bbox_to_anchor=(1, .5), loc='center left', borderaxespad=0.5)
+		ax_simp.set_xticks(list(range(len(simplices))))
+		ax_simp.set_ylabel('number of simplices')
+
+	else:
+		ax_spikes = fig.add_subplot(2,1,1)
+		ax_volts = fig.add_subplot(2,1,2)
+
+	ax_spikes.invert_yaxis()
 	ax_spikes.scatter(spikes['times'], spikes['senders'], s=1, marker="+")
 	ax_spikes.set_ylabel('neuron index')
-	ax_spikes.set_xticks([])
 	ax_spikes.set_ylim(1,nnum)
-	plt.gca().invert_yaxis()
-	ax_spikes.set_xlim(1,exp_length)
+	ax_spikes.set_xlim(0,exp_length-1)
 	
-	ax_volts = fig.add_subplot(212)
 	v = ax_volts.imshow(np.transpose(np.array(volts).reshape(int(exp_length-1),nnum)), cmap=plt.cm.Spectral_r, interpolation='None', aspect="auto")
+	ax_volts.invert_yaxis()
 	ax_volts.set_ylabel('neuron index')
 	ax_volts.set_xlabel('time in ms')
 	
-	fig.colorbar(v, ax=[ax_spikes,ax_volts], pad=0.03, fraction=0.02, orientation='vertical', label="voltage")
+	fig.colorbar(v, ax=ax_volts, pad=0.03, fraction=0.02, orientation='vertical', label="voltage")
 	plt.savefig(root+'report_'+timestamp+'.png')
