4d2cd2fb5827a93a5b08c59be4f0041058165e9f
matsantoro <matteo.santoro@epfl.ch>
2021-05-10 07:49:40 -0400

    Removed unnecessary print

--- nsimplex/nsimplex_sim_runner.py
+++ nsimplex/nsimplex_sim_runner.py
@@ -20,7 +20,6 @@ def build_matrices(path, n):
         matrix = triu_from_array(np.array(array), n)
         matrix = np.triu(np.ones((n,n)).astype(np.bool))+matrix.T
         identifier = ''.join([str(int(elem)) for elem in array])
-        print(identifier)
         save_path = path.with_name(path.stem + identifier + '.npy')
         (Path('structure') / save_path).parent.mkdir(parents=True, exist_ok=True)
         np.save(Path('structure') / save_path, matrix, allow_pickle = True)
